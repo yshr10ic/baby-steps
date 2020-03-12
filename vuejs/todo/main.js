eb5be8b369a2f1bd49f39516d0c4aca708a7d974
yshr10ic <yshr10ic@gmail.com>
2020-03-12 22:36:42 +0900

    done step2

--- vuejs/todo/main.js
+++ vuejs/todo/main.js
@@ -1,3 +1,20 @@
+var STORAGE_KEY = 'todos-vuejs-demo'
+var todoStorage = {
+    fetch: function() {
+        var todos = JSON.parse(
+            localStorage.getItem(STORAGE_KEY) || '[]'
+        )
+        todos.forEach(function(todo, index) {
+            todo.id = index
+        });
+        todoStorage.uid = todos.length
+        return todos
+    },
+    save: function(todos) {
+        localStorage.save(STORAGE_KEY, JSON.stringify(todos))
+    }
+}
+
 const app = new Vue({
     el: '#app',
     data: {
