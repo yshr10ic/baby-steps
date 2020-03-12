099aecd08e109eb74e666b3f7d77678b31be18aa
yshr10ic <yshr10ic@gmail.com>
2020-03-12 22:44:35 +0900

    done step5

--- vuejs/todo/main.js
+++ vuejs/todo/main.js
@@ -1,26 +1,26 @@
 var STORAGE_KEY = 'todos-vuejs-demo'
 var todoStorage = {
-    fetch: function() {
-        var todos = JSON.parse(
-            localStorage.getItem(STORAGE_KEY) || '[]'
-        )
-        todos.forEach(function(todo, index) {
-            todo.id = index
-        });
-        todoStorage.uid = todos.length
-        return todos
-    },
-    save: function(todos) {
-        localStorage.save(STORAGE_KEY, JSON.stringify(todos))
-    }
+  fetch: function() {
+    var todos = JSON.parse(
+      localStorage.getItem(STORAGE_KEY) || '[]'
+    )
+    todos.forEach(function(todo, index) {
+      todo.id = index
+    })
+    todoStorage.uid = todos.length
+    return todos
+  },
+  save: function(todos) {
+    localStorage.save(STORAGE_KEY, JSON.stringify(todos))
+  }
 }
 
 const app = new Vue({
-    el: '#app',
-    data: {
-        // 使用するデータ
-    },
-    methods: {
-        // 使用するメソッド
-    }
+  el: '#app',
+  data: {
+    todos: []
+  },
+  methods: {
+    // 使用するメソッド
+  }
 })
