e1cc18faa304368eb2d87d51ca0793955ed267ad
yshr10ic <yshr10ic@gmail.com>
2020-03-13 09:57:43 +0900

    done step11

--- vuejs/todo/main.js
+++ vuejs/todo/main.js
@@ -19,7 +19,15 @@ const app = new Vue({
   el: '#app',
 
   data: {
-    todos: []
+    todos: [],
+
+    options: [
+      { value: -1, label: 'すべて' },
+      { value: 0, label: '作業中' },
+      { value: 1, label: '完了' }
+    ],
+
+    current: -1
   },
 
   watch: {
