d0cd0a3ca6d295cee9c0910088cc015659233e54
yshr10ic <yshr10ic@gmail.com>
2020-03-13 22:12:56 +0900

    done

--- vuejs/chat/src/main.js
+++ vuejs/chat/src/main.js
@@ -1,7 +1,20 @@
 import Vue from 'vue'
 import App from './App.vue'
+import firebase from 'firebase'
 
 Vue.config.productionTip = false
+
+var firebaseConfig = {
+  apiKey: "",
+  authDomain: "",
+  databaseURL: "",
+  projectId: "",
+  storageBucket: "",
+  messagingSenderId: "",
+  appId: ""
+};
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
 
 new Vue({
   render: h => h(App),
