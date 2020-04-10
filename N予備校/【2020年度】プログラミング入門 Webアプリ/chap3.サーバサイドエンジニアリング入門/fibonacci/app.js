bb606d09d16eba700f1be17c634edd6cf2d830d1
yshr10ic <yshr10ic@gmail.com>
2020-04-10 19:16:42 +0900

    アルゴリズムの改善 指数オーダーのfib関数の実装

---
+++ N予備校/【2020年度】プログラミング入門 Webアプリ/chap3.サーバサイドエンジニアリング入門/fibonacci/app.js
@@ -0,0 +1,17 @@
+'use stinct';
+
+function fib(n) {
+    if (n === 0) {
+        return 0;
+    } else if (n === 1) {
+        return 1;
+    } else {
+        return fib(n - 1) + fib(n - 2)
+    }
+    return null;
+}
+
+const length = 40;
+for (let i = 0; i <= length; i++) {
+    console.log(fib(i));
+}
