ceb34fa56fb7f36d7c3c2599d14f1d7f94f75fe3
yshr10ic <yshr10ic@gmail.com>
2020-04-11 14:43:49 +0900

    done HTTPのメソッド

--- N予備校/【2020年度】プログラミング入門 Webアプリ/chap3.サーバサイドエンジニアリング入門/node-js-http/index.js
+++ N予備校/【2020年度】プログラミング入門 Webアプリ/chap3.サーバサイドエンジニアリング入門/node-js-http/index.js
@@ -3,13 +3,34 @@
 const http = require('http');
 const server = http
     .createServer((req, res) => {
+        const now = new Date();
         console.info(
-            '[' + new Date() + '] Requested by ' + req.connection.remoteAddress
+            '[' + now + '] Requested by ' + req.connection.remoteAddress
         );
         res.writeHead(200, {
             'Content-Type': 'text/plain; charset=utf-8'
         });
-        res.write(req.headers['user-agent']);
+        switch (req.method) {
+            case 'GET':
+                res.write('GET ' + req.url);
+                break;
+            case 'POST':
+                res.write('POST ' + req.url);
+                let rawData = '';
+                req
+                    .on('data', (chunk) => {
+                        rawData += chunk;
+                    })
+                    .on('end', () => {
+                        console.info('[' + now + '] Data posted: ' + rawData)
+                    });
+                break;
+            case 'DELETE':
+                res.write('DELETE ' + req.url);
+                break;
+            default:
+                break;
+        }
         res.end();
     })
     .on('error', e => {
