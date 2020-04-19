d5a8b9d8379c25837c49cc862604c79792a78319
yshr10ic <yshr10ic@gmail.com>
2020-04-19 21:45:29 +0900

    update if.go

--- go/A_Tour_of_Go/Basics2/if.go
+++ go/A_Tour_of_Go/Basics2/if.go
@@ -15,6 +15,8 @@ func sqrt(x float64) string {
 func pow(x, n, lim float64) float64 {
 	if v := math.Pow(x, n); v < lim {
 		return v
+	} else {
+		fmt.Printf("%g >= %g\n", v, lim)
 	}
 	return lim
 }
