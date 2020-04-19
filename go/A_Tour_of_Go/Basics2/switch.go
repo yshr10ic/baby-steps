bfd6129c93d9898aad88cf5c82e4bc257e47b0f7
yshr10ic <yshr10ic@gmail.com>
2020-04-19 22:12:45 +0900

    update switch.go

--- go/A_Tour_of_Go/Basics2/switch.go
+++ go/A_Tour_of_Go/Basics2/switch.go
@@ -31,4 +31,15 @@ func main() {
 	default:
 		fmt.Println("Too far away.")
 	}
+
+	fmt.Print("\n")
+	t := time.Now()
+	switch {
+	case t.Hour() < 12:
+		fmt.Println("Good morning!")
+	case t.Hour() < 17:
+		fmt.Println("Good afternoon.")
+	default:
+		fmt.Println("Good evening.")
+	}
 }
