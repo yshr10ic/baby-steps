9377322c5f069b4bc844182531b3b97733b6d0bc
yshr10ic <yshr10ic@gmail.com>
2020-04-19 19:24:06 +0900

    update variables.go

--- go/A_Tour_of_Go/Basics/variables.go
+++ go/A_Tour_of_Go/Basics/variables.go
@@ -1,6 +1,9 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"reflect"
+)
 
 var c, python, java bool
 var x, y int = 1, 2
@@ -8,7 +11,12 @@ var x, y int = 1, 2
 func main() {
 	var i int
 	var a, b, d = true, false, "str"
+	k := 3
+	l := "str2"
+	m := false
 	fmt.Println(i, c, python, java)
 	fmt.Println(x, y)
 	fmt.Println(a, b, d)
+	fmt.Println(k, l, m)
+	fmt.Println(reflect.TypeOf(k), reflect.TypeOf(l), reflect.TypeOf(m))
 }
