6b4b48055e7763e4ccf6c542206940168bf3a3e0
yshr10ic <yshr10ic@gmail.com>
2020-04-19 19:10:33 +0900

    update variables.go

--- go/A_Tour_of_Go/Basics/variables.go
+++ go/A_Tour_of_Go/Basics/variables.go
@@ -3,8 +3,12 @@ package main
 import "fmt"
 
 var c, python, java bool
+var x, y int = 1, 2
 
 func main() {
 	var i int
+	var a, b, d = true, false, "str"
 	fmt.Println(i, c, python, java)
+	fmt.Println(x, y)
+	fmt.Println(a, b, d)
 }
