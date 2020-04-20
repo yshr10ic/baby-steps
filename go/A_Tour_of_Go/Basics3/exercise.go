5b3edbc373a648d5d8002842342e7a03c275312a
yshr10ic <yshr10ic@gmail.com>
2020-04-20 20:00:26 +0900

    add exercise.go

---
+++ go/A_Tour_of_Go/Basics3/exercise.go
@@ -0,0 +1,18 @@
+package main
+
+import "fmt"
+
+func fibonacci() func() int {
+	a, b := 1, 0
+	return func() int {
+		a, b = b, a + b
+		return a
+	}
+}
+
+func main() {
+	f := fibonacci()
+	for i := 0; i < 15; i++ {
+		fmt.Println(f())
+	}
+}
