99447542c72be57632d1f8aa808525a096c28fc0
yshr10ic <yshr10ic@gmail.com>
2020-04-20 19:58:22 +0900

    update function.go

--- go/A_Tour_of_Go/Basics3/function.go
+++ go/A_Tour_of_Go/Basics3/function.go
@@ -9,6 +9,14 @@ func compute(fn func(float64, float64) float64) float64 {
 	return fn(3, 4)
 }
 
+func adder() func(int) int {
+	sum := 0
+	return func(x int) int {
+		sum += x
+		return sum
+	}
+}
+
 func main() {
 	hypot := func(x float64, y float64) float64 {
 		return math.Sqrt(x * x + y * y)
@@ -17,4 +25,14 @@ func main() {
 
 	fmt.Println(compute(hypot))
 	fmt.Println(compute(math.Pow))
+
+	fmt.Println("")
+
+	pos, neg := adder(), adder()
+	for i := 0; i < 10; i++ {
+		fmt.Println(
+			pos(i),
+			neg(-2*i),
+		)
+	}
 }
