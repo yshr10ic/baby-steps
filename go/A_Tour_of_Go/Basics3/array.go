35180cbc051b0cc2739a05fb2885ab4de2c7fc69
yshr10ic <yshr10ic@gmail.com>
2020-04-20 17:16:01 +0900

    update array.go

--- go/A_Tour_of_Go/Basics3/array.go
+++ go/A_Tour_of_Go/Basics3/array.go
@@ -12,6 +12,21 @@ func main() {
 	primes := [6]int{2, 3, 5, 7, 11, 13}
 	fmt.Println(primes)
 
+	// slices
 	var s []int = primes[1:4]
 	fmt.Println(s)
+
+	// slices-pointers
+	names := [4]string{
+		"John", "Paul", "George", "Ringo",
+	}
+	fmt.Println(names)
+
+	b := names[0:2]
+	c := names[1:3]
+	fmt.Println(b, c)
+
+	c[0] = "Alice"
+	fmt.Println(b, c)
+	fmt.Println(names)
 }
