0ded75bba3f3f8f30f761037bc83e4e2cfcb6978
yshr10ic <yshr10ic@gmail.com>
2020-04-20 17:45:30 +0900

    update slice-len-cap.go

--- go/A_Tour_of_Go/Basics3/slice-len-cap.go
+++ go/A_Tour_of_Go/Basics3/slice-len-cap.go
@@ -8,6 +8,19 @@ func main() {
 	printSlice(s[:0])
 	printSlice(s[:4])
 	printSlice(s[2:])
+
+	// make
+	a := make([]int, 5)
+	printSlice(a)
+
+	b := make([]int, 0, 5)
+	printSlice(b)
+
+	c := b[:2]
+	printSlice(c)
+
+	d := c[2:5]
+	printSlice(d)
 }
 
 func printSlice(s []int) {
