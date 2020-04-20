dd3a11f16384a0e3186f53564159b46dedb4016f
yshr10ic <yshr10ic@gmail.com>
2020-04-20 19:28:33 +0900

    update ramge.go

--- go/A_Tour_of_Go/Basics3/range.go
+++ go/A_Tour_of_Go/Basics3/range.go
@@ -8,4 +8,13 @@ func main() {
 	for i, v := range pow {
 		fmt.Printf("2 ** %d = %d\n", i, v)
 	}
+
+	pow2 := make([]int, 10)
+	for i := range pow2 {
+		pow2[i] = 1 << uint(i)
+	}
+
+	for _, value := range pow2 {
+		fmt.Printf("%d\n", value)
+	}
 }
