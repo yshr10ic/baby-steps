3b559a7938cc211151e1f229b34de88559fe7a87
yshr10ic <yshr10ic@gmail.com>
2020-04-25 19:37:19 +0900

    add thread model

--- django/djangorestframework/chat_project/chats/models.py
+++ django/djangorestframework/chat_project/chats/models.py
@@ -1,3 +1,10 @@
 from django.db import models
+import users.models
 
-# Create your models here.
+
+class Thread(models.Model):
+    owner = models.ForeignKey(users.models.User, on_delete=models.CASCADE)
+    title = models.CharField(max_length=200)
+    created_at = models.DateTimeField('作成日時', auto_now_add=True)
+    updated_at = models.DateTimeField('更新日時', auto_now=True)
+    is_public = models.BooleanField(default=False)
